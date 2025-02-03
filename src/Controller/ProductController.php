e6e0e558e916a35c38c4a579152ca2c4d6f93333
Chandrasekaraa Ramadoss Ganesh <94929823+RGChandrasekaraa@users.noreply.github.com>
2025-02-03 18:56:57 +1000

    validation completed

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,6 +7,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ProductRepository;
 use App\Entity\Product;
+use App\Form\ProductType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class ProductController extends AbstractController
 {
@@ -27,8 +30,24 @@ final class ProductController extends AbstractController
     }
 
     #[Route('/products/new', name: 'product_new')]
-    public function new(): Response
+    public function new(Request $request, EntityManagerInterface $manager) : Response
     {
-        return $this->render('product/new.html.twig');
+        $product = new Product();
+
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager -> persist($product);
+            $manager -> flush();
+
+            return $this->redirectToRoute('product_show', [
+                'id' => $product->getId(),
+            ]);
+        }
+        return $this->render('product/new.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
