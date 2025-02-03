52d1008cea8589f488b86454f07f0e9af1a60f62
Chandrasekaraa Ramadoss Ganesh <94929823+RGChandrasekaraa@users.noreply.github.com>
2025-02-03 17:33:52 +1000

    till forms completed

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,14 +5,30 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ProductRepository;
+use App\Entity\Product;
 
 final class ProductController extends AbstractController
 {
     #[Route('/products', name: 'product_index')]
-    public function index(): Response
+    public function index(ProductRepository $repository): Response
     {
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products' => $repository->findAll(),
         ]);
+    }
+
+    #[Route('/products/{id<\d+>}', name: 'product_show')]
+    public function show(Product $product): Response
+    {
+        return $this->render('product/show.html.twig',[
+            'product' => $product,
+        ]);
+    }
+
+    #[Route('/products/new', name: 'product_new')]
+    public function new(): Response
+    {
+        return $this->render('product/new.html.twig');
     }
 }
